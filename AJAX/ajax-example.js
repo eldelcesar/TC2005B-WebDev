af32545a77c9912dadd19e2a638bbe002d281052
Cesar Espinosa Michel <cesarjr.espinosa@gmail.com>
2023-02-22 17:16:52 -0600

    Agregando las practicas de fundamentos web

---
+++ AJAX/ajax-example.js
@@ -0,0 +1,9 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+}
---
+++ HTML-CSS-JS/main.js
@@ -0,0 +1,5 @@
+// JavaScript
+function changeColor(newColor) {
+   var elem = document.getElementById('the-title');
+   elem.style.color = newColor;
+}
