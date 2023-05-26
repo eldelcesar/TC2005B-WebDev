ac13e23087e6f40b78fec8af989931d3e5dc328b
Cesar Espinosa Michel <cesarjr.espinosa@gmail.com>
2023-05-25 18:27:20 -0600

    Agregando Lab 6 con componentes de ReactJS

---
+++ Servicios-Web/Lab6-Componentes/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
